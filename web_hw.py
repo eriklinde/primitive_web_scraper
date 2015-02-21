da87b1e34822dd3fbf0d6b273c6ec60233cd7a54
Erik Linde <erik.linde@gmail.com>
2015-02-21 00:18:37 -0500

    Simplified test app

--- web_hw.py
+++ web_hw.py
@@ -1,10 +1,8 @@
-from flask import render_template
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def articles():
     return "Hello!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
